a86ae3231765b8b15e7c4a231205f0a355b7b1a4
Erik Skinnari <erikskinnari@gmail.com>
2020-03-10 23:35:46 +0100

    Computer swap

---
+++ Assets/Scripts/ParallaxBG.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBG : MonoBehaviour
+{
+    [SerializeField]
+    public Transform playerTransform;
+    private Vector3 lastPlayerPosition;
+
+    private void Start()
+    {
+        playerTransform = GetComponent<PlayerBehaviour>().transform;
+        lastPlayerPosition = playerTransform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 deltaMovement = playerTransform.position - lastPlayerPosition;
+        float parallaxMultiplyer = .5f;
+        transform.position += deltaMovement * parallaxMultiplyer;
+        lastPlayerPosition = playerTransform.position;
+    }
+
+}
